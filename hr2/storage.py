386d25f64fba1825b88acd4faf8eb54c1849a2c5
Hiromichi Matsushima <hylom@iMac4k.local>
2021-06-12 23:12:14 +0900

    storage: fix transaction to work without native transaction

--- hr2/storage.py
+++ hr2/storage.py
@@ -1,57 +1,58 @@
 """storage - data storage class"""
 
 class Storage():
+    _results = []
+    _in_transaction = False
+
     def __init__(self, engine, default_engine):
         self.storage = engine[default_engine]
-        self._results = []
-        self._in_transaction = False
 
     def get(self, key, default=None):
         try:
             r = self.storage[key]
         except KeyError:
             r = default
-        if not self.storage.has_transaction:
-            self._results.append(r)
+        if Storage._in_transaction:
+            Storage._results.append(r)
         return r
 
     def clear(self):
         r = self.storage.clear()
-        if not self.storage.has_transaction:
-            self._results.append(r)
+        if Storage._in_transaction:
+            Storage._results.append(r)
         return r
 
     def __getitem__(self, key):
         r = self.storage[key]
-        if not self.storage.has_transaction:
-            self._results.append(r)
+        if Storage._in_transaction:
+            Storage._results.append(r)
         return r
 
     def __setitem__(self, key, value):
         self.storage[key] = value
-        if not self.storage.has_transaction:
-            self._results.append(None)
+        if Storage._in_transaction:
+            Storage._results.append(None)
 
     def __delitem__(self, key):
         del self.storage[key]
-        if not self.storage.has_transaction:
-            self._results.append(None)
+        if Storage._in_transaction:
+            Storage._results.append(None)
 
     def __enter__(self):
         if self.storage.has_transaction:
             return self.storage.__enter__()
-        self._in_transaction = True
-        self._results = []
+        Storage._in_transaction = True
+        Storage._results = []
         return self    
 
     def __exit__(self, exc_type, exc_value, traceback):
         if self.storage.has_transaction:
             return self.storage.__exit__(exc_type, exc_value, traceback)
-        self._in_transaction = False
+        Storage._in_transaction = False
         return self    
 
     @property
     def results(self):
         if self.storage.has_transaction:
             return self.storage.results
-        return self._results
+        return Storage._results
